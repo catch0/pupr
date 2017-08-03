0681563afd9e915d433e81b91a6060c37c7ebee1
Spence Peacock <spencerpeacock@gmail.com>
2017-08-03 12:01:56 -0500

    merged pup controllers

--- server/controllers/pups.js
+++ server/controllers/pups.js
@@ -1,20 +1,47 @@
-let mongoose = require('mongoose');
-let Pup = mongoose.model("Pup");
+let mongoose= require('mongoose');
+let User=mongoose.model('User');
+let Pups= mongoose.model('Pup');
 
-class PupsController {
-    create(req, res){
-        Pop.find({name: req.body.name},(err,pup)=>{
-            if(err){return res.json(err)}
-            else if(!user){
-                Pup.create(req.body,(err,pup)=>{
-                    if(err){return res.json(err)}
-                    return res.json(pup)
-                })
-            }
-            else{
-                return res.json(pop)
-            }
-        })
-    }
+
+
+class PupsController{
+  index(req, res){
+    Pup.find({}).populate('user').exec((err, pups)=> {
+      if(err){ return res.json(err)}
+      return res.json(pups);
+  })
+ }
+ create(req,res){
+
+   Pup.create(req.body, (err, pup)=>{
+     if(err){return res.json(err)}
+     User.findByIdAndUpdate(
+       req.body.user,
+       {$push: {pups: pup._id}},
+       {new: true},
+       (err, user)=>{
+         if(err){return res.json(err)}
+         return res.json(pup);
+       })
+   })
+ }
+
+ show(req, res){
+   Pup.findById(req.params.id,(err, pup)=>{
+   if(err){return res.json(err)}
+    return res.json(pup);
+
+   })
   }
+  delete(req, res){
+  Pup.findByIdAndRemove(req.params.id, (err,pup) => {
+          if(err) {
+            return res.json(err);
+          }
+          console.log(pup)
+          return res.json(pup);
+      });
+  }
+
+}
 module.exports = new PupsController();
